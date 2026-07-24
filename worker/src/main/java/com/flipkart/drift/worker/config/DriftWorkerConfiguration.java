43d5a0a2b1c287bff108a0c2a26ed79e6da3c03f
Sodu2301 <89843514+Sodu2301@users.noreply.github.com>
2026-07-24 20:39:21 +0530

    Update DriftWorkerConfiguration.java

--- worker/src/main/java/com/flipkart/drift/worker/config/DriftWorkerConfiguration.java
+++ worker/src/main/java/com/flipkart/drift/worker/config/DriftWorkerConfiguration.java
@@ -22,7 +22,7 @@ public class DriftWorkerConfiguration extends Configuration {
     private StaticCacheRefreshConfig staticCacheRefreshConfig;
     @NotNull
     private CacheMaxEntriesConfig cacheMaxEntriesConfig;
-    @NotNull
+    
     private HbaseNamespaceConfig hbaseNamespaceConfig;
     @NotNull
     private String hbasePropertiesPath;
